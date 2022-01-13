3e21b63e4d39af2ef214ea528fa11c310103ac3c
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2022-01-13 19:41:11 +0500

    this program display value of array

---
+++ example1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int number[5];
+    number[0]=1;
+    number[1]=15;
+    number[2]=20;
+    number[3]=15;
+    number[4]=25;
+    cout<<"first element in array:"<<number[0]<<endl;
+    cout<<"second element in array:"<<number[1]<<endl;
+    cout<<"third element in array:"<<number[2]<<endl;
+    cout<<"forth element in array:"<<number[3]<<endl;
+    cout<<"fivth element in array:"<<number[4]<<endl;
+
+}
