5681125b67bcf7abf33b953d99a485ab907a2541
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2022-01-13 19:44:02 +0500

    program will display 1st and last element of array
    see problem three on lab manual

---
+++ example3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int number[5];
+    cout<<"enter frist element of array:";
+    cin >> number[0];
+    cout<<"enter 2nd element of array:";
+    cin >> number[1];
+    cout<<"enter 3rd element of array:";
+    cin >> number[2];
+    cout<<"enter forth element of array:";
+    cin >> number[3];
+    cout<<"enter fifth element of array:";
+    cin >> number[4];
+    cout<<"first element in array:"<<number[0]<<endl;
+    cout<<"fifth element in array:"<<number[4]<<endl;
+
+}
