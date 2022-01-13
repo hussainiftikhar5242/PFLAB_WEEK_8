beccd87818a3f47cc53c5e51c07f489cc728267c
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2022-01-13 20:04:06 +0500

    program replace the letter with relative letter
    like a replace to b, b to c and so on

---
+++ task4.cpp
@@ -0,0 +1,123 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    string name;
+    cout<<"enter a name:";
+    cin>>name;
+    int i=0;
+    while( name[i] != '\0')
+    {
+        if(name[i] == 'a')
+        {
+            name[i]='b';
+        }
+        else if(name[i] == 'b')
+        {
+            name[i]='c';
+        }
+        else if(name[i] == 'c')
+        {
+            name[i]='d';
+        }
+        else if(name[i] == 'd')
+        {
+            name[i]='e';
+        }
+        if(name[i] == 'e')
+        {
+            name[i]='f';
+        }
+        else if(name[i] == 'f')
+        {
+            name[i]='g';
+        }
+        else if(name[i] == 'a')
+        {
+            name[i]='b';
+        }
+        else if(name[i] == 'g')
+        {
+            name[i]='h';
+        }
+        else if(name[i] == 'h')
+        {
+            name[i]='i';
+        }
+        else if(name[i] == 'i')
+        {
+            name[i]='j';
+        }
+        else if(name[i] == 'j')
+        {
+            name[i]='k';
+        }
+        else if(name[i] == 'k')
+        {
+            name[i]='l';
+        }
+        else if(name[i] == 'l')
+        {
+            name[i]='m';
+        }
+        else if(name[i] == 'm')
+        {
+            name[i]='n';
+        }
+        else if(name[i] == 'n')
+        {
+            name[i]='o';
+        }
+        else if(name[i] == 'o')
+        {
+            name[i]='p';
+        }
+        else if(name[i] == 'p')
+        {
+            name[i]='q';
+        }
+        else if(name[i] == 'q')
+        {
+            name[i]='r';
+        }
+        else if(name[i] == 'r')
+        {
+            name[i]='s';
+        }
+        else if(name[i] == 's')
+        {
+            name[i]='t';
+        }
+        else if(name[i] == 't')
+        {
+            name[i]='u';
+        }
+        else if(name[i] == 'u')
+        {
+            name[i]='v';
+        }
+        else if(name[i] == 'v')
+        {
+            name[i]='w';
+        }
+        else if(name[i] == 'w')
+        {
+            name[i]='x';
+        }
+        else if(name[i] == 'x')
+        {
+            name[i]='y';
+        }
+        else if(name[i] == 'y')
+        {
+            name[i]='z';
+        }
+        else if(name[i] == 'z')
+        {
+            name[i]='a';
+        }
+        i++;
+
+    }
+    cout<<name;
+}
