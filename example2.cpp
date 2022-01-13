564162720488eb5c513e33e412b891f859d8c7ba
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2022-01-13 19:42:00 +0500

    this program will display on sepecified value
    see lab manual example 2

---
+++ example2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int number[5];
+    number[0]=1;
+    number[1]=50;
+    number[2]=20;
+    number[3]=15;
+    number[4]=25;
+    cout<<"second element in array:"<<number[1]<<endl;
+    cout<<"forth element in array:"<<number[3]<<endl;
+
+}
